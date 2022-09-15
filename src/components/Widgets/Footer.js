9ff118b447ec1e07a8648bc83936c2e01bda727c
ahmed ashraf <48288103+ahmedraslan28@users.noreply.github.com>
2022-09-15 05:52:36 +0200

    footer udated

--- src/components/Widgets/Footer.js
+++ src/components/Widgets/Footer.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "../../Styles/Footer.css";
+import { BsGlobe2 } from "react-icons/bs";
 function Footer() {
   return (
     <footer>
-      <div class="foot-title">
-        <div className="container-top">
-          <p>
-            Top companies choose Udemy Business to build in-demand career
-            skills.
-          </p>
+      <div className="foot-title">
+        <div className="foot-title-txt">
+          <span>
+            Top companies choose
+            <a className="udemy-buisness">Udemy Business</a> to build in-demand
+            career skills.
+          </span>
+        </div>
+        <div className="foot-title-imgs">
           <img
             class="foot-img"
             alt="Nasdaq"
@@ -38,7 +42,7 @@ function Footer() {
             src="https://s.udemycdn.com/partner-logos/v4/netapp-light.svg"
           ></img>
           <img
-            class="foot-img"
+            class="foot-img last"
             alt="Eventbrite"
             height="44"
             width="115"
@@ -46,29 +50,43 @@ function Footer() {
           ></img>
         </div>
       </div>
-      <div className="footer-catiogreis">
-        <a class="cat-a">Udemy Business</a>
-        <a class="cat-a"> Careers</a>
-        <a class="cat-a">Terms</a>
-        <a class="cat-a">Teach on Udemy</a>
-        <a class="cat-a">Blog</a>
-        <a class="cat-a">Privacy policy</a>
-        <a class="cat-a">Get the app</a>
-        <a class="cat-a">Help and Support</a>
-        <a class="cat-a">Cookie settings</a>
-        <a class="cat-a">About us</a>
-        <a class="cat-a">Affiliate</a>
-        <a class="cat-a">Sitemap</a>
-        <a class="cat-a">Contact us</a>
-        <a class="cat-a">Investors</a>
-        <a class="cat-a">Accessibility statement</a>
-      </div>
-      <div>
-        <img
-          class="img-log"
-          src="https://www.udemy.com/staticx/udemy/images/v7/logo-udemy-inverted.svg"
-          alt="Udemy"
-        ></img>
+      <div className="others">
+        <div className="categories-btn">
+          <div className="foot-categories">
+            <a class="cat-a">Udemy Business</a>
+            <a class="cat-a"> Careers</a>
+            <a class="cat-a">Terms</a>
+            <a class="cat-a">Teach on Udemy</a>
+            <a class="cat-a">Blog</a>
+            <a class="cat-a">Privacy policy</a>
+            <a class="cat-a">Get the app</a>
+            <a class="cat-a">Help and Support</a>
+            <a class="cat-a">Cookie settings</a>
+            <a class="cat-a">About us</a>
+            <a class="cat-a">Affiliate</a>
+            <a class="cat-a">Sitemap</a>
+            <a class="cat-a">Contact us</a>
+            <a class="cat-a">Investors</a>
+            <a class="cat-a">Accessibility statement</a>
+          </div>
+          <div className="foot-btn">
+            <button>
+              <BsGlobe2 className="pl-5" color="white" size={17} />
+              <span className="grb">English</span>
+            </button>
+          </div>
+        </div>
+        <div className="img-copyrights">
+          <div className="foot-img-cont">
+            <img
+              src="https://www.udemy.com/staticx/udemy/images/v7/logo-udemy-inverted.svg"
+              alt="Udemy"
+              width={91.0667}
+              height={34.2}
+            ></img>
+          </div>
+          <div className="copyright">© 2022 Udemy, Inc.</div>
+        </div>
       </div>
     </footer>
   );
